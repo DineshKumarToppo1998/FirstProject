537c60ca615860f8fc630dc14857446490b2abca
DineshKumarToppo1998 <dk9853600415@gmail.com>
2022-09-03 14:01:18 +0530

    Refactor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("First Program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+
+
+
     }
 }
